8ede3f7cdd1973631511efe5ac8adba062f2fdb6
ismansiete <isman7@gmail.com>
2016-11-20 13:00:58 +0100

    More comments.2

--- bottle_server.py
+++ bottle_server.py
@@ -3,14 +3,21 @@ import bottle
 import os
 import argparse
 
+
+# Looking for arguments "-H" for host and "-p" for port. argparse deals with "-h" help argument automatically.
 parser = argparse.ArgumentParser()
 parser.add_argument('-H', help='Server. I. e. \'localhost\'', default='localhost')
 parser.add_argument('-p', help='Port. I. e. \'8080\'', default='8080')
 args = parser.parse_args()
 
+# Creating a Bottle app instance.
 app = Bottle()
+
+# Using os module to know were the server is running.
 abspath = os.path.abspath(".")
-print "The absolute path to program is: {}".format(abspath)
+print "The absolute path to server program is: {}".format(abspath)
+
+# Bottle Functions start here #
 
 
 @app.route('/static/<filename>')
